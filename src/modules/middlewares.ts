d196e8f3c53f25da57d9443efff1129a52a04d52
bhumit070 <ganatrabhoomit070@gmail.com>
2024-12-08 16:53:05 +0530

    add: cors

--- src/modules/middlewares.ts
+++ src/modules/middlewares.ts
@@ -1,10 +1,15 @@
 import express, { Application } from "express";
+import cors from "cors";
 import routes from "./index";
 import { handleApiError, routeNotFound } from "./common/controllers";
 import { PromiseHandler } from "./common/middlewares";
 
 export function registerMiddlewares(app: Application) {
-  app.use(express.json());
+  app.use(express.json()).use(
+    cors({
+      origin: "*",
+    })
+  );
 }
 
 export function registerRoutes(app: Application) {
